bad97c79319bde61dd27aff8b18694b42b047de3
roguexray007 <hardxray007@gmail.com>
2019-07-12 01:34:51 +0530

    database connection done

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -14,23 +15,38 @@ const (
 	dbPASSWORD = "ks"
 )
 
-// func init() {
-// 	var err error
-// 	db, err = gorm.Open("mysql", dbUSER+":"+dbPASSWORD+"@/"+dbNAME+"?charset=utf8&parseTime=True&loc=Local")
-// 	if err != nil {
-// 		panic("failed to connect to database")
-// 	}
+// type urlRecord struct {
+// 	gorm.Model
+// 	Name             string `json:"name"`
+// 	CrawlTimeOut     int    `json:"crawlTimeOut`
+// 	Frequency        int    `json:"frequency`
+// 	FailureThreshold int    `json:"failureThreshold`
+// 	Status           int    `json:"status"`
 // }
 
-func main() {
-	router := gin.Default()
-
-	app := router.Group("api/healthcheck")
-	{
-		app.GET("/", func(c *gin.Context) {
-
-		})
+func dbInit() {
+	var err error
+	db, err = gorm.Open("mysql", dbUSER+":"+dbPASSWORD+"@/"+dbNAME+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to database")
+	} else {
+		fmt.Println("successfully connected")
 	}
+}
 
-	router.Run()
+func main() {
+	dbInit()
+
+	// router := gin.Default()
+
+	// app := router.Group("api/healthcheck")
+	// {
+	// 	app.GET("/", func(c *gin.Context) {
+	// 		c.JSON(200, gin.H{
+	// 			"message": "hello world",
+	// 		})
+	// 	})
+	// }
+
+	// router.Run()
 }
